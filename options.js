8562c549676a270e73d17ec12c15a43c016bbe07
Koen de Bont <koen-debont@hotmail.com>
2016-02-22 10:46:07 +0100

    veranderonh

---
+++ options.js
@@ -0,0 +1,32 @@
+var options = {
+	game : {
+		fps : 30
+	},
+	player : {
+		player1 : null,
+		player2 : null,
+		turn : 0
+	},
+	background : {
+		image : 'night'
+	},
+	default : {
+		actiontext : "",
+		actiondamage : "",
+		actionhealing : "",
+		actionenergy : ""
+	}
+}
+var game = {
+	player : {
+		1 : null,
+		2 : null,
+		turn : 0
+	},
+	action : {
+		name : "",
+		damge : "",
+		healing : "",
+		energy : ""
+	}
+}
---
+++ powers.js
@@ -0,0 +1,310 @@
+var sietse = {
+	1 : {
+		name : 'Sarcasme',
+		energy : {
+			cost : 20,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 20
+		},
+		healing : {
+			min : 0,
+			max : 5
+		}
+	},
+	2 : {
+		name : 'Machtsmisbruik',
+		energy : {
+			cost : 30,
+			generate : 0
+		},
+		damage : {
+			min : 20,
+			max : 30
+		},
+		healing : {
+			min : 0,
+			max : 10
+		}
+	},
+	3 : {
+		name : 'Killerapplicatie',
+		energy : {
+			cost : 40,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 25
+		},
+		healing : {
+			min : 10,
+			max : 20
+		}
+	},
+	4 : {
+		name : 'C++',
+		energy : {
+			cost : 25,
+			generate : 0
+		},
+		damage : {
+			min : 10,
+			max : 20
+		},
+		healing : {
+			min : 0,
+			max : 5
+		}
+	},
+	5 : {
+		name : 'Rusten',
+		energy : {
+			cost : 0,
+			generate : 50
+		},
+		damage : {
+			min : 0,
+			max : 0
+		},
+		healing : {
+			min : 10,
+			max : 15
+		}
+	}
+}
+var fedde = {
+	1 : {
+		name : 'Motiveren',
+		energy : {
+			cost : 0,
+			generate : 30
+		},
+		damage : {
+			min : 0,
+			max : 0
+		},
+		healing : {
+			min : 20,
+			max : 25
+		}
+	},
+	2 : {
+		name : 'Bootstrap',
+		energy : {
+			cost : 0,
+			generate : 10
+		},
+		damage : {
+			min : 10,
+			max : 20
+		},
+		healing : {
+			min : 10,
+			max : 15
+		}
+	},
+	3 : {
+		name : 'PHP',
+		energy : {
+			cost : 10,
+			generate : 0
+		},
+		damage : {
+			min : 10,
+			max : 15
+		},
+		healing : {
+			min : 0,
+			max : 5
+		}
+	},
+	4 : {
+		name : 'jQuery',
+		energy : {
+			cost : 10,
+			generate : 0
+		},
+		damage : {
+			min : 5,
+			max : 15
+		},
+		healing : {
+			min : 0,
+			max : 5
+		}
+	},
+	5 : {
+		name : 'Judo',
+		energy : {
+			cost : 50,
+			generate : 0
+		},
+		damage : {
+			min : 20,
+			max : 30
+		},
+		healing : {
+			min : 0,
+			max : 0
+		}
+	}
+}
+
+var fer = {
+	1 : {
+		name : 'Spellingscontrole',
+		energy : {
+			cost : 15,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 25
+		},
+		healing : {
+			min : 0,
+			max : 5
+		}
+	},
+	2 : {
+		name : 'Documentatie',
+		energy : {
+			cost : 10,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 25
+		},
+		healing : {
+			min : 0,
+			max : 10
+		}
+	},
+	3 : {
+		name : 'PGO',
+		energy : {
+			cost : 20,
+			generate : 0
+		},
+		damage : {
+			min : 20,
+			max : 25
+		},
+		healing : {
+			min : 5,
+			max : 15
+		}
+	},
+	4 : {
+		name : 'C++',
+		energy : {
+			cost : 10,
+			generate : 0
+		},
+		damage : {
+			min : 10,
+			max : 15
+		},
+		healing : {
+			min : 0,
+			max : 0
+		}
+	},
+	5 : {
+		name : 'Rusten',
+		energy : {
+			cost : 0,
+			generate : 50
+		},
+		damage : {
+			min : 0,
+			max : 0
+		},
+		healing : {
+			min : 0,
+			max : 15
+		}
+	}
+}
+
+var piet = {
+	1 : {
+		name : 'Miscommunicatie',
+		energy : {
+			cost : 10,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 20
+		},
+		healing : {
+			min : 0,
+			max : 0
+		}
+	},
+	2 : {
+		name : 'Documentatie',
+		energy : {
+			cost : 20,
+			generate : 0
+		},
+		damage : {
+			min : 15,
+			max : 20
+		},
+		healing : {
+			min : 5,
+			max : 20
+		}
+	},
+	3 : {
+		name : 'Trolley',
+		energy : {
+			cost : 0,
+			generate : 10
+		},
+		damage : {
+			min : 0,
+			max : 5
+		},
+		healing : {
+			min : 15,
+			max : 25
+		}
+	},
+	4 : {
+		name : 'MySite',
+		energy : {
+			cost : 0,
+			generate : 5
+		},
+		damage : {
+			min : 5,
+			max : 10
+		},
+		healing : {
+			min : 5,
+			max : 10
+		}
+	},
+	5 : {
+		name : 'Late reactie',
+		energy : {
+			cost : 0,
+			generate : 5
+		},
+		damage : {
+			min : 10,
+			max : 20
+		},
+		healing : {
+			min : 5,
+			max : 15
+		}
+	}
+}
